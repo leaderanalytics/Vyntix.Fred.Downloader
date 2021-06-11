50c648b422ead5d7a5526ee9bcdfb4127ceb9e9d
Sam Wheat <SWheat@sdgecontractor.com>
2021-06-11 13:55:30 -0700

    Add Logging

--- Downloader.Blazor/Server/Program.cs
+++ Downloader.Blazor/Server/Program.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace Downloader.Blazor.Server
 {
@@ -14,7 +16,38 @@ namespace Downloader.Blazor.Server
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            string logRoot = null;
+
+            if (env == "Development")
+                logRoot = "c:\\serilog\\Vyntix.FRED.Downloader\\log";
+            else
+                logRoot = "..\\..\\LogFiles\\Vyntix.Web\\log";   // Create logs in D:\home\LogFiles
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(logRoot, rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information, buffered: false)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Vyntix.FRED.Downloader - Program.Main started.");
+                Log.Information("Environment is: {env}", env);
+                Log.Information("Log files will be written to {logRoot}", logRoot);
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+                System.Threading.Thread.Sleep(2000);
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
