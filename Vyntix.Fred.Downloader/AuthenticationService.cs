71e58c4f2f0abf15155ba386276760c71622e794
Sam <sam.wheat@outlook.com>
2024-05-06 05:31:28 -0700

    Add AuthenticationService

---
+++ Vyntix.Fred.Downloader/AuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaderAnalytics.Vyntix.Fred.Downloader;
+
+public class AuthenticationService : BaseService, IAuthenticationService
+{
+    public AuthenticationService(FREDStagingDb db, IAPI_Manifest downloaderServices, IFredClient fredClient, ILogger<CategoriesService> logger, Action<string> statusCallback) : base(db, downloaderServices, fredClient, logger, statusCallback)
+    {
+
+    }
+
+    public async Task<bool> IsAPI_KeyValid() => await fredClient.IsAPI_KeyValid();
+    
+}
